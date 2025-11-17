b35357d8cadd3b249cb711d7edd673caf9ed5a2d
Jakub Majerčík <89073674+iakupp@users.noreply.github.com>
2025-11-17 11:33:03 +0100

    added small padding left on ReviewBox section

--- src/components/ReviewBox.tsx
+++ src/components/ReviewBox.tsx
@@ -10,7 +10,7 @@ const ReviewBox = ({ id, title, date, message }: ReviewBoxProps) => {
     <div
       key={id}
       id="review"
-      className=" w-[337px] h-[207px] md:w-[550px] xl:w-[750px] mx-auto bg-(--review-box-bg) border-l-[5px] border-l-(--accent-color) pt-[20px] px-[20px] xl:mr-[4.8em] xl:mt-[1.5em] shadow-[7px_5px_19px_-5px_rgba(0,0,0,0.5)]"
+      className=" w-[337px] h-[207px] md:w-[550px] xl:w-[750px] mx-auto bg-(--review-box-bg) border-l-[5px] border-l-(--accent-color) pt-[2%] pl-[40px] xl:mr-[4.8em] xl:mt-[1.5em] shadow-[7px_5px_19px_-5px_rgba(0,0,0,0.5)]"
     >
       <h1 className="text-[20px] text-(--accent-text)">{title}</h1>
       <p className="text-[16px] text-(--gray-text) mb-[20px]">{date}</p>
